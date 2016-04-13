c1d5d4b9211d3f65d33da143d259056199f82925
danger12001 <paul@projectcodex.co>
2016-04-13 12:17:05 +0200

    total trips

---
+++ CapeTown.js
@@ -0,0 +1,53 @@
+exports.capeTownTaxis = function(){
+  var capeTownTaxis = [
+    {
+      "RegistrationNumber": "CA 123 456",
+      "Route": "Cape Town - Bellville",
+      "Fare": 13,
+      "Trips": 9
+    },
+    {
+      "RegistrationNumber": "CA 234 567",
+      "Route": "Cape Town - Gugulethu",
+      "Fare": 12,
+      "Trips": 11
+    },
+    {
+      "RegistrationNumber": "CA 123 456",
+      "Route": "Cape Town - Gugulethu",
+      "Fare": 12,
+      "Trips": 11
+    },
+    {
+      "RegistrationNumber": "CA 345 678",
+      "Route": "Cape Town - Langa",
+      "Fare": 8,
+      "Trips": 13
+    },
+    {
+      "RegistrationNumber": "CA 345 678",
+      "Route": "Cape Town - Cape Town",
+      "Fare": 13,
+      "Trips": 10
+    }
+  ];
+  return capeTownTaxis;
+};
+exports.totalTrips = function(capeTownTaxis){
+var trips = [];
+var total = 0;
+for( var trip in capeTownTaxis){
+  var amountTrip = capeTownTaxis[trip].Trips;
+  trips.push(amountTrip);
+}
+for(var x in trips){
+  total += trips[x];
+}
+return total;
+};
+exports.lowestTrips = function(capeTownTaxis){
+  for(var trip in capeTownTaxis){
+    var amountTrip = capeTownTaxis[trip].Trips;
+    console.log(amountTrip);
+  }
+};
---
+++ test/tests.js
@@ -0,0 +1,113 @@
+var assert = require('assert');
+var ct = require('../CapeTown');
+
+describe("capeTownTaxis",function(){
+  it("should return the maps of the taxis",function(){
+    var result = ct.capeTownTaxis();
+    assert.deepEqual(result,[
+  {
+    "RegistrationNumber": "CA 123 456",
+    "Route": "Cape Town - Bellville",
+    "Fare": 13,
+    "Trips": 9
+  },
+  {
+    "RegistrationNumber": "CA 234 567",
+    "Route": "Cape Town - Gugulethu",
+    "Fare": 12,
+    "Trips": 11
+  },
+  {
+    "RegistrationNumber": "CA 123 456",
+    "Route": "Cape Town - Gugulethu",
+    "Fare": 12,
+    "Trips": 11
+  },
+  {
+    "RegistrationNumber": "CA 345 678",
+    "Route": "Cape Town - Langa",
+    "Fare": 8,
+    "Trips": 13
+  },
+  {
+    "RegistrationNumber": "CA 345 678",
+    "Route": "Cape Town - Cape Town",
+    "Fare": 13,
+    "Trips": 10
+  }
+]);
+  });
+});
+
+describe("totalTrips",function(){
+  it("should return the total trips of all the taxis",function(){
+    var result = ct.totalTrips([
+  {
+    "RegistrationNumber": "CA 123 456",
+    "Route": "Cape Town - Bellville",
+    "Fare": 13,
+    "Trips": 9
+  },
+  {
+    "RegistrationNumber": "CA 234 567",
+    "Route": "Cape Town - Gugulethu",
+    "Fare": 12,
+    "Trips": 11
+  },
+  {
+    "RegistrationNumber": "CA 123 456",
+    "Route": "Cape Town - Gugulethu",
+    "Fare": 12,
+    "Trips": 11
+  },
+  {
+    "RegistrationNumber": "CA 345 678",
+    "Route": "Cape Town - Langa",
+    "Fare": 8,
+    "Trips": 13
+  },
+  {
+    "RegistrationNumber": "CA 345 678",
+    "Route": "Cape Town - Cape Town",
+    "Fare": 13,
+    "Trips": 10
+  }]);
+    assert.equal(result,54);
+  });
+});
+describe("lowestTrips",function(){
+  it("should return the lowest number of trips any taxi gave",function(){
+    var result = ct.lowestTrips([
+  {
+    "RegistrationNumber": "CA 123 456",
+    "Route": "Cape Town - Bellville",
+    "Fare": 13,
+    "Trips": 9
+  },
+  {
+    "RegistrationNumber": "CA 234 567",
+    "Route": "Cape Town - Gugulethu",
+    "Fare": 12,
+    "Trips": 11
+  },
+  {
+    "RegistrationNumber": "CA 123 456",
+    "Route": "Cape Town - Gugulethu",
+    "Fare": 12,
+    "Trips": 11
+  },
+  {
+    "RegistrationNumber": "CA 345 678",
+    "Route": "Cape Town - Langa",
+    "Fare": 8,
+    "Trips": 13
+  },
+  {
+    "RegistrationNumber": "CA 345 678",
+    "Route": "Cape Town - Cape Town",
+    "Fare": 13,
+    "Trips": 10
+  }]);
+    assert.equal(result,"");
+  });
+});
